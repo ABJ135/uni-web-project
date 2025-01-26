6427377d316d9079c94055ca14029bf44fa8697b
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2025-01-26 20:20:38 +0500

    attendence schema

---
+++ model/Attendence.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const {Schema} = require("mongoose")
+
+const attendenceSchema =new Schema ({
+    employee_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    date: { type: Date, required: true },
+    clock_in: { type: Date },
+    clock_out: { type: Date },
+    status: { type: String, enum: ['present', 'absent', 'late'], required: true },
+  }, { timestamps: true });
+  
+  const attendence = mongoose.model("employee",employeeSchema)
+
+  module.exports = attendence
